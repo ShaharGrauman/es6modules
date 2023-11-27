af9ec0685cb80a61744742479e9c2dca92a88e0a
Shahar Grauman <graumanoz@gmail.com>
2023-11-27 19:44:12 +0200

    .

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,5 @@
 /// <reference path="../../node_modules/@types/jquery/index.d.ts" />
 
-// import jquery from "C:/Users/graum/FullStack/typescript/es6modules/node_modules/jquery/dist/jquery.js"
-
-// const $: JQueryStatic = jquery
-
 import { products } from './products.js'
 
 const init = (num:number) => {
